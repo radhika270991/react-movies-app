68f78e3fbca3d1a0dda31cb849af2d27747df0c5
radhikamandil <mandil.radhika@gmail.com>
2020-01-12 22:41:04 +0530

    Added grid list for ‘Upcoming Movies’

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,7 +2,10 @@ import React, {Component} from 'react';
 import './Home.css';
 import Header from '../../common/header/Header';
 import { withStyles } from '@material-ui/core/styles';
-import movieData from '../../common/movieData';
+import moviesData from '../../common/movieData';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 const styles = theme => ({
     root: {
@@ -31,6 +34,15 @@ class Home extends Component{
                 <div className={classes.upcomingMoviesHeading}>
                     <b>Upcoming Movies</b>
                 </div>
+                <GridList cols={5} className={classes.gridListUpcomingMovies}>
+                    {moviesData.map(movie => (
+                        <GridListTile key={movie.id}>
+                            <img src={movie.poster_url} alt={movie.title}></img>
+                            <GridListTileBar title={movie.title}>
+                            </GridListTileBar>
+                        </GridListTile>
+                    ))}
+                </GridList>
             </div>
         )
             
