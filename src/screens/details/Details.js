23de221fbe0b632ebf3d938a8efa7eade0772d7c
radhikamandil <mandil.radhika@gmail.com>
2020-01-15 20:32:08 +0530

    Added poster, title and genres on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,33 +1,47 @@
 import React, { Component } from 'react';
 import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
+import Typography from '@material-ui/core/Typography';
+import './Details.css';
 
 
 class Details extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            movie:{}
+            movie: {}
         }
     }
 
-    componentDidMount(){
+    componentWillMount() {
         let currentState = this.state;
         currentState.movie = moviesData.filter((mov) => {
-                return mov.id === this.props.movieId
+            return mov.id === this.props.movieId
         })[0];
-        this.setState({currentState});
+        this.setState({ currentState });
         console.log(this.state);
     }
 
     render() {
+        let movie = this.state.movie;
         return (
             <div className="details">
                 <Header />
                 <div className='flex-containerDetails'>
-                    <div className="leftDetails"></div>
-                    <div className="middleDetails"></div>
+                    <div className="leftDetails">
+                        <img src={movie.poster_url} />
+                    </div>
+                    <div className="middleDetails">
+                        <div>
+                            <Typography variant="headline" component="h2">{movie.title} </Typography>
+                        </div>
+                        <div>
+                            <Typography>
+                                <span className="bold">Genres: </span> {movie.genres.join(',')}
+                            </Typography>
+                        </div>
+                    </div>
                     <div className="rightDetails"></div>
                 </div>
             </div>
